e4ff3bf1860d0b8b98b640d558879ada2a7257f7
JBindio <bindio@naver.com>
2022-05-13 10:05:24 +0900

    220513 커밋완료

--- day3/12.R_Study_매트릭스02.R
+++ day3/12.R_Study_매트릭스02.R
@@ -14,3 +14,15 @@ y
 
 m2 <- rbind(m1, y)
 m2
+
+z
+z[2,1:3] # 2행의 값 중 1~3열에 있는 값
+z[1,c(1,2,4)] # 1행의 값 중 1, 2, 4열에 있는 값
+z[1:2,] # 1~2행에 있는 모든 값
+z[,c(1,4)] # 1, 4열에 있는 모든 값
+
+score <- matrix(c(90,85,69,78,85,96,49,95,90,80,70,60), nrow=4)
+score
+rownames(score) <- c('John','Tom','Mark','Jane')
+colnames(score) <- c('English','Math','Science')
+score
